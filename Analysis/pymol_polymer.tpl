c6f56cb08cf7cd424a127c1e90ede78a6ae5fb25
Ludwig Ohl <Ludwig.Ohl@uni-due.com>
2017-06-13 11:52:38 +0200

    fixed and improved pymol visualisation

--- Analysis/pymol_polymer.tpl
+++ Analysis/pymol_polymer.tpl
@@ -1,7 +1,7 @@
 {% for run in pymol._poly_end_poses() -%}
 MODEL {{loop.index}}
 {% for coords in run -%}
-{{'ATOM   {:>4}  {}   LIG  {:>4}    {:>8.3f}{:>8.3f}{:>8.3f}  1.00  0.00            {} '.format(coords['id'], coords['ele'], coords['id'], coords['x'], coords['y'], coords['z'], coords['ele']) }}
+{{'ATOM   {:>4}  {}   {:<3} A{:>4}    {:>8.3f}{:>8.3f}{:>8.3f}  1.00  0.00           {:<2}'.format(coords['id'], coords['ele'], coords['res_name'], coords['id'], coords['x'], coords['y'], coords['z'], coords['ele']) }}
 {% endfor %}
 ENDMDL
 {% endfor %}
--- Analysis/pymol_visualisation.py
+++ Analysis/pymol_visualisation.py
@@ -13,11 +13,11 @@ class DensityContainer(object):
         super(DensityContainer, self).__init__()
         self.sim = simulation
         self.monomer_id = monomer_id
-        self.db_folder = self.sim.db_path.parent.absolute().resolve()
         self.margin = margin
         self.resolution = resolution
+        self.db_folder = self.sim.db_path.parent.absolute().resolve()
         
-        self.box, self.box_size, self.map = self._create_empty_map()
+        #self.box, self.box_size, self.map = self._create_empty_map()
 
     @property
     def monomer_id(self):
@@ -35,8 +35,8 @@ class DensityContainer(object):
             m_id = list(self.sim._particle_ids['polymer'])
         else:
             raise ValueError('monomer_id must be int or list or "all".')
-            
-        if set(m_id) <= set(self.sim._particle_ids['polymer']):
+
+        if not set(m_id) <= set(self.sim._particle_ids['polymer']):
             raise ValueError("Some Ids are not ids of monomer. Choose from %s" % self.sim._particle_ids['polymer'])
         else:
             self._monomer_id = m_id
@@ -46,16 +46,22 @@ class DensityContainer(object):
         return self._margin
 
     @margin.setter
-    def margin(self):
-        raise UserWarning('This attribute should not be changed after object creation.')
+    def margin(self, value):
+        if not hasattr(self, '_margin'):
+            self._margin = value
+        else:
+            raise UserWarning('This attribute should not be changed after object creation.')
     
     @property
     def resolution(self):
         return self._resolution
 
     @resolution.setter
-    def resolution(self):
-        raise UserWarning('This attribute should not be changed after object creation.')
+    def resolution(self, value):
+        if not hasattr(self, '_resolution'):
+            self._resolution = value
+        else:
+            raise UserWarning('This attribute should not be changed after object creation.')
 
 
     def _create_empty_map(self):
@@ -65,31 +71,35 @@ class DensityContainer(object):
         box = self.sim._calc_protein_box(self.margin)
         self.box_size = (box[1] - box[0])
         epi_map = np.zeros(np.ceil(self.box_size/float(self.resolution))+1, np.int32)
-        return box, box_size, epi_map
+        self.box, self.box_size, self.map = box, box_size, epi_map
         
-    def _map_path(self, monomer_id):
+    def _map_path(self):
         '''Create a unique path name for the 3d-density map.
         '''
         prefix = 'map_'
-        mono_str = 'ID-%s_' % 'X'.join(map(str, monomer_id))
+        mono_str = 'ID-%s_' % 'X'.join(map(str, self.monomer_id))
         margin_str = 'M-%.02f_' % (round(self.margin,2))
         resolution_str = 'R-%.02f_' % (round(self.resolution,2))
         suffix = '.dx'
         name = ''.join([prefix, mono_str, margin_str, resolution_str, suffix])
         return self.db_folder.joinpath(name).absolute().resolve()
 
-    def _create_atom_type_filter(self, particle_order, monomer_id):
+    def _create_atom_type_filter(self, particle_order, monomer_id=None):
+        if not monomer_id:
+            monomer_id = self.monomer_id
         mask = np.in1d(particle_order, monomer_id)
         iterator = it.cycle(mask)
         def atom_type_filter(whatever):
             return iterator.next()
         return atom_type_filter
 
-    def _add_run_to_epitopsy_map(self, run_id, monomer_id):
+    def _add_run_to_epitopsy_map(self, run_id, monomer_id=None):
         """Add all mapped coordinates to epitopsy grid.
         """
+        if not monomer_id:
+            monomer_id = self.monomer_id
         particle_order = self.sim._parse.load_traj_type_order()
-        type_filter = self._create_atom_type_filter(particle_order, self.monomer_id)
+        type_filter = self._create_atom_type_filter(particle_order)
         offset = self.box[0]
         traj_iterator = self.sim._parse.trajectory(run_id)
         monomer_coords = it.ifilter(type_filter, traj_iterator)
@@ -98,29 +108,31 @@ class DensityContainer(object):
             self.map[idx[0],idx[1],idx[2]] += 1
         return self.map
     
-    def create_epitopsy_map(self, monomer_id, margin=20, resolution=1.0, norm=None, save=False):
+    def _create_normalized(self, norm_type='max'):
+        if norm_type == 'max':
+            self.normed_map = self.map / self.map.max().astype(np.float)
+        else:
+            self.normed_map = self.map
+
+    def create_epitopsy_map(self, norm='max', save=False):
         '''Create a 3-dimensional density map of one monomers. This density map 
         
         input:
         monomer_id: 
-        norm: if true, rescales all values by deviding through the maximum value of the map. [bool]
+        norm: Options: 'max', None [string]
         '''
-        box, epi_box = self._create_empty_epitopsy_map(self.margin, self.resolution)
+        self._create_empty_map()
         for run in tqdm.tqdm_notebook(self.sim, leave=False, desc='Adding Job data'):
-            self._add_run_to_epitopsy_map(run.Id, monomer_id)
-        if norm:
-            normed_epi_box = epi_box / epi_box.max().astype(np.float)
-        else:
-            normed_epi_box = epi_box
+            self._add_run_to_epitopsy_map(run.Id, self.monomer_id)
+        self._create_normalized(norm_type=norm)
         if save:
             self.save()
-        return box, normed_epi_box
+        return self.box, self.normed_map
 
-    def save(self):
+    def save(self,):
         '''Save data to the dx format via the DXBox object of epitopsy.
         '''
-        box_size = (self.box[1] - self.box[0])
-        dx_obj = dx.DXBox(box=self.map, meshsize=box_size/self.map.shape, offset=self.box[0])
+        dx_obj = dx.DXBox(box=self.normed_map, meshsize=self.box_size/self.normed_map.shape, offset=self.box[0])
         path = self._map_path().as_posix()
         dx_obj.write(path)
         return path
@@ -143,12 +155,17 @@ class PymolVisualisation(object):
         self.pymol_handle = pym.connect(ip='localhost')
     
     def _poly_end_poses(self):
+        '''Create generator of data for polymer-pdb via the pymol_polymer.tpl
+        Each yield (run) the coordinates of the polymer are updated, while the constant
+        information is left unchanged
+        '''
         elements = {id_: name for id_, name in zip(self.sim._particle_ids['polymer'], self.atom_names)}
         mask = np.in1d(self.sim._parse.load_traj_type_order(), self.sim._particle_ids['polymer'])
-        pose_list_type = [('id', '>i2'), ('ele', '|S1'), ('x', np.float), ('y', np.float), ('z', np.float)]
+        pose_list_type = [('id', '>i2'), ('ele', '|S1'), ('res_name', '|S3'), ('x', np.float), ('y', np.float), ('z', np.float)]
         pose_data = np.zeros(mask.sum(), dtype=pose_list_type)
         pose_data['id'] = np.arange(1, mask.sum()+1)
         pose_data['ele'] = map(lambda x:elements[x], self.sim[0].coordinates()['end'][mask]['atom_type'])
+        pose_data['res_name'] = self.sim.sequence['monomer']
         for run in self.sim:
             coords = run.coordinates()['end'][mask]
             pose_data['x'] = coords['x']
@@ -157,6 +174,10 @@ class PymolVisualisation(object):
             yield pose_data
 
     def setup(self):
+        '''Setup a new pymol scene. First, delete any previous setup.
+        Load the protein in pymol in cartoon style,
+        and highlight the active site.
+        '''
         self.pymol_handle.delete('all')
         self.pymol_handle.load(self.protein_path.as_posix())
         self.pymol_handle.show_as('cartoon')
@@ -168,13 +189,12 @@ class PymolVisualisation(object):
         self.pymol_handle.color('red', 'active_site')
 
     def add_dx(self, monomer_id, margin=20.0, resolution=1.5):
-        path = self._map_path(monomer_id, margin=margin, resolution=resolution)
-        if not path.exists():
-            density = DensityContainer(monomer_id, margin=margin, resolution=resolution)
-            path = density.save()
-            monomer_id = density.monomer_id
-        self.pymol_handle.load(path.as_posix())
-        pymol_name = path.name.rstrip('.dx')
+        density = DensityContainer(self.sim, monomer_id, margin=margin, resolution=resolution)
+        dx_path = density._map_path()
+        if not dx_path.exists():
+            density.create_epitopsy_map(norm='max', save=True)
+        self.pymol_handle.load(dx_path.as_posix())
+        pymol_name = dx_path.name.rstrip('.dx')
         return monomer_id, pymol_name
 
     def show_isosurface(self, dx_info, level=0.5):
