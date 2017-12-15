41263b61802d0f12c396c5bde85c6a63ab737c67
niklas <niklas.toetsch@stud.uni-due.de>
2017-12-15 14:48:12 +0100

    Only check active site if there is a protein in the system

--- Analysis/job.py
+++ Analysis/job.py
@@ -10,6 +10,7 @@ import pymol_visualisation
 import sim_run
 import numerics as num_
 import PolyLibScan.Database.db as DB
+import PolyLibScan.Tools.config as cfg
 
 warnings.filterwarnings("ignore")
 
@@ -29,7 +30,15 @@ class Job(bayes.Job):
         self.trajectory_order = self._parse.traj_type_order
         self.sequence = self._parse.sequence
         self.weights = dict(self._parse.weights)
-        self.active_site = self._parse.active_site
+
+        # set self.active_site if there's a protein present
+        self.config = cfg.JobConfig(self.db_path.parent.joinpath('config_with_setup.yml').as_posix())
+        if 'stoichiometry' in self.config.sim_parameter:
+            if self.config.sim_parameter['stoichiometry'][0] > 0:
+                self.active_site = self._parse.active_site
+        else:
+            self.active_site = self._parse.active_site
+
         self._runs = self._read_runs(with_pymol=with_pymol)
         self.particle_ids = self._get_particle_ids()
         self._charge = None
